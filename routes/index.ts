7a21d3795db6330b6a8c647484a7ce3531ba56ac
nieyaolong@gmail.com <nie753951>
2017-01-12 01:31:45 +0800

    private info

--- routes/index.ts
+++ routes/index.ts
@@ -4,7 +4,23 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-    res.render('index', { title: 'Express' });
+    const bridegroom = '聂耀龙';
+    const bride = '高颖慧';
+    const date = new Date('2017-1-18');
+    const feast_date = new Date('2017-1-19');
+    const feast_address = '山东省禹城市明珠大酒店';
+    const location = '山东禹城';
+    const address = '山东省禹城市金穗大酒店';
+    res.render('index', {
+        title: '欢迎光临',
+        bridegroom: bridegroom,
+        bride: bride,
+        date: date.toLocaleDateString(),
+        feast_date: feast_date.toLocaleDateString(),
+        feast_address: feast_address,
+        location: location,
+        address: address
+    });
 });
 
 module.exports = router;
