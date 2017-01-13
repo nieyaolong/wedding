f836e38c20a9b0e9104f7c2bbb1c650453c28f07
nieyaolong@gmail.com <nie753951>
2017-01-13 22:51:55 +0800

    invitation

--- routes/index.ts
+++ routes/index.ts
@@ -12,7 +12,7 @@ router.get('/', (req, res) => {
     const location = '山东禹城';
     const address = '山东省禹城市金穗大酒店';
     res.render('index', {
-        title: '欢迎光临',
+        title: `#${bridegroom}和${bride}的婚礼`,
         bridegroom: bridegroom,
         bride: bride,
         date: date.toLocaleDateString(),
